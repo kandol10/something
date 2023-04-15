172c7ce916044e5e1589f8499cee649f9efe225d
kandol10 <prabbi.kandola@hotmail.co.uk>
2023-04-15 03:58:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
 
 # Replace the placeholders below with your actual values
-github_repo_owner = 'your-github-username'
+github_repo_owner = 'kandol10'
 github_repo_name = 'Something'
-github_file_path = 'main/a'
+github_file_path = 'index.html'
 
 # Retrieve the raw content of the file from GitHub
-response = requests.get(f'https://raw.githubusercontent.com/{github_repo_owner}/{github_repo_name}/main/a')
+response = requests.get(f'https://raw.githubusercontent.com/{github_repo_owner}/{github_repo_name}/main/{github_file_path}')
 
 # Print the content of the file
 print(response.content.decode('utf-8'))
